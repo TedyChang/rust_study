c830cdf906b3e2e12d051d997e612da6977ed4f5
askePhoenix <aistarteverything@gmail.com>
2022-06-29 19:44:14 +0900

    study #3

--- push.sh
+++ push.sh
@@ -4,10 +4,10 @@ echo -n "issue num ? "
 read ISSUENO
 echo -n "y or n Continue?"
 read YN
-if [[ "$YN" =~ ^[Yy] ]]
+if [[ "$YN" =~ ^[Nn] ]]
 then
+else
   git add .
   git commit -m "study #${ISSUENO}"
   git push origin main
-else
 fi
