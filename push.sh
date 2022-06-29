9fb8d9ff29477fcbefe2be7b4b43b46da4aa5476
askePhoenix <aistarteverything@gmail.com>
2022-06-29 19:31:37 +0900

    study #

--- push.sh
+++ push.sh
@@ -2,7 +2,13 @@
 
 git add .
 ISSUE_NO=""
-read -p "Enter issue no: " ISSUE_NO
+read "?ISSUE_NO Continue?"
 
-git commit -m "study #$ISSUE_NO"
-git push origin main
+
+read "?YN y or n Continue?"
+if [[ "$YN" =~ ^[Yy] ]]
+then
+else
+  git commit -m "study #$ISSUE_NO"
+  git push origin main
+fi
